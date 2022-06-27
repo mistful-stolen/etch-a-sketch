ea08115396262a42c8f68001b102dc85bfdf5a42
mistful <willthedragonslayer@protonmail.com>
2022-06-27 18:07:04 +0200

    Add range

--- main.js
+++ main.js
@@ -1,22 +1,55 @@
 const container = document.getElementById("container");
+const range = document.getElementById("range");
 const subContainer = document.createElement("div");
 const element = document.createElement("div");
+const COLOR_BLACK = "black";
+subContainer.setAttribute("id", "subContainer");
+element.setAttribute("id", "element");
 
-createGrid();
+createGrid(4);
+let idElements = document.querySelectorAll("#element");
+gridHover();
+changeGrid();
 
-function createGrid() {
-  createFirstNode();
-  cloneNodes();
+function changeGrid() {
+  range.addEventListener("mouseup", () => {
+    const idSubContainer = document.querySelectorAll("#subContainer");
+    idElements = document.querySelectorAll("#element");
 
-  function cloneNodes() {
-    for (let i = 0; i < 15; i++) {
+    idSubContainer.forEach((item) => {
+      item.remove();
+    });
+    idElements.forEach((item) => {
+      item.remove();
+    });
+
+    createGrid(range.value);
+    idElements = document.querySelectorAll("#element");
+    gridHover();
+  });
+}
+
+function gridHover() {
+  idElements.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "black";
+    });
+  });
+}
+
+function createGrid(gridSize) {
+  createFirstNode(gridSize);
+  cloneNodes(gridSize);
+
+  function cloneNodes(userGridNumber) {
+    for (let i = 1; i < userGridNumber; i++) {
       container.appendChild(subContainer.cloneNode(true));
     }
   }
-  function createFirstNode() {
+  function createFirstNode(userGridNumber) {
     container.appendChild(subContainer);
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < userGridNumber; i++) {
       subContainer.appendChild(element.cloneNode(true));
     }
   }
